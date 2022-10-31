396358842df7d05472e344a351abb5b3f820c3db
twojnarowski <tym17@24x7.pl>
2022-10-31 20:44:45 +0100

    Added Identity

--- MVCUserRoleManager/Program.cs
+++ MVCUserRoleManager/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVCUserRoleManager.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MVCUserRoleManagerContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCUserRoleManagerContextConnection' not found.");
+
+builder.Services.AddDbContext<MVCUserRoleManagerContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MVCUserRoleManagerContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
