f8fce14fe5d45bddc01aa858d9806d6099f2830e
twojnarowski <tym17@24x7.pl>
2022-10-31 21:05:16 +0100

    Added classes for User and Role, inheriting from Identity-User and-Role

---
+++ MVCUserRoleManager/Areas/Identity/Data/Role.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace MVCUserRoleManager.Areas.Identity.Data
+{
+    public class Role : IdentityRole
+    {
+        public Role()
+        {
+        }
+
+        public Role(string roleName) : base(roleName)
+        {
+        }
+
+        public Role(string roleName, string badge) : base(roleName)
+        {
+            Badge = badge;
+        }
+
+        [StringLength(6, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        public string? Badge { get; set; }
+    }
+}
---
+++ MVCUserRoleManager/Areas/Identity/Data/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace MVCUserRoleManager.Areas.Identity.Data
+{
+    public class User : IdentityUser
+    {
+        public User() : base()
+        {
+
+        }
+        public User(string userName, string firstName, string lastName) : base(userName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        [StringLength(35)]
+        public string? FirstName { get; set; }
+
+        [StringLength(35)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public IEnumerable<string> RolesForUser { get; set; }
+    }
+}
