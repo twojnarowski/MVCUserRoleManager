ac3433bfdd8876f49c057156d2b6a41f01cc7f69
twojnarowski <tym17@24x7.pl>
2022-10-31 21:01:16 +0100

    Added navigation to User- and Role-Manager

--- MVCUserRoleManager/Controllers/HomeController.cs
+++ MVCUserRoleManager/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace MVCUserRoleManager.Controllers
             return View();
         }
 
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Roles()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
