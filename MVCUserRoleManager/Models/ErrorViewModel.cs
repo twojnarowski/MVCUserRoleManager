eb309556c89ab2fb445c6dd2eab35bd56bb823b8
twojnarowski <tym17@24x7.pl>
2022-10-31 20:40:53 +0100

    Dodaj pliki projektów.

---
+++ MVCUserRoleManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCUserRoleManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
